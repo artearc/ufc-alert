032cfa2770dad6f94e7f2faec43689baf553ca79
Alex Ruiz Chazarra <57905898+artearc@users.noreply.github.com>
2026-06-09 17:54:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
 import os
+from datetime import datetime
 
 TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
+
+hora = datetime.utcnow().strftime("%H:%M:%S UTC")
 
 requests.post(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
     json={
         "chat_id": CHAT_ID,
-        "text": "🥊 Tu bot funciona correctamente"
+        "text": f"⏰ Workflow ejecutado: {hora}"
     }
 )
